4d959c8612bc335d89dea2e1b53c0e2f66ea55a4
David Hasenson <davidhasenson@gmail.com>
2026-08-17 12:07:30 +0200

    updated

--- src/main/java/org/example/resttest/HelloController.java
+++ src/main/java/org/example/resttest/HelloController.java
@@ -1,4 +1,13 @@
 package org.example.resttest;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+
+        return "Hej";
+    }
 }
--- src/test/java/org/example/resttest/ResttestApplicationTests.java
+++ src/test/java/org/example/resttest/ResttestApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ResttestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
